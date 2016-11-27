e73e6bd44df2f801bb9f44a8e2079508a41b650e
Bart Janssens <bart@bartjanssens.org>
2016-11-27 22:35:55 +0100

    [split C++] Windows build cleanup and 32 bit fixes

--- cmake/WinDeployQt.cmake
+++ cmake/WinDeployQt.cmake
@@ -1,2 +1,2 @@
 set(ENV{VCINSTALLDIR} "${VC_DIR}")
-execute_process(COMMAND ${QtCore_location}/windeployqt --qmldir "${QML_DIR}" "${CMAKE_INSTALL_PREFIX}/lib${LIBDIR_SUFFIX}/qmlwrap.dll")
+execute_process(COMMAND ${QtCore_location}/windeployqt --qmldir "${QML_DIR}" "${CMAKE_INSTALL_PREFIX}/lib/qmlwrap.dll")
