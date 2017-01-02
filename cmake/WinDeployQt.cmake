686f68a907dc5eb2180e3b604b8936d1dda01a02
Bart Janssens <bart@bartjanssens.org>
2017-01-02 20:59:57 +0100

    [split C++] Windows build updates

--- cmake/WinDeployQt.cmake
+++ cmake/WinDeployQt.cmake
@@ -1,2 +1,2 @@
-set(ENV{VCINSTALLDIR} "${VC_DIR}")
-execute_process(COMMAND ${QtCore_location}/windeployqt --qmldir "${QML_DIR}" "${CMAKE_INSTALL_PREFIX}/lib/qmlwrap.dll")
+#set(ENV{VCINSTALLDIR} "${VC_DIR}")
+execute_process(COMMAND ${QtCore_location}/windeployqt --release --qmldir "${QML_DIR}" "${CMAKE_INSTALL_PREFIX}/lib/${LIB_PREFIX_STR}qmlwrap.dll")
