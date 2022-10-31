6ff88f7853dae48b0abb0a44fe12c859d6df2d86
Bart Janssens <bart@bartjanssens.org>
2022-10-31 16:01:30 +0100

    First working version of the TableModel

--- wrap_qml.cpp
+++ wrap_qml.cpp
@@ -16,14 +16,12 @@
 #include "julia_api.hpp"
 #include "julia_canvas.hpp"
 #include "julia_display.hpp"
+#include "julia_itemmodel.hpp"
 #include "julia_painteditem.hpp"
 #include "julia_property_map.hpp"
 #include "julia_signals.hpp"
-#include "listmodel.hpp"
-#include "modeldata.hpp"
 #include "opengl_viewport.hpp"
 #include "makie_viewport.hpp"
-#include "tablemodel.hpp"
 
 #include "jlqml.hpp"
 
@@ -41,10 +39,8 @@ template<> struct SuperType<QQuickView> { using type = QQuickWindow; };
 template<> struct SuperType<QTimer> { using type = QObject; };
 template<> struct SuperType<qmlwrap::JuliaPaintedItem> { using type = QQuickItem; };
 template<> struct SuperType<QAbstractItemModel> { using type = QObject; };
-template<> struct SuperType<QAbstractListModel> { using type = QAbstractItemModel; };
 template<> struct SuperType<QAbstractTableModel> { using type = QAbstractItemModel; };
-template<> struct SuperType<qmlwrap::ListModel> { using type = QAbstractListModel; };
-template<> struct SuperType<qmlwrap::TableModel> { using type = QAbstractTableModel; };
+template<> struct SuperType<qmlwrap::JuliaItemModel> { using type = QAbstractTableModel; };
 
 }
 
@@ -206,13 +202,77 @@ struct WrapQList
   template<typename TypeWrapperT>
   void operator()(TypeWrapperT&& wrapped)
   {
-    typedef typename TypeWrapperT::type WrappedT;
+    using WrappedT = typename TypeWrapperT::type;
     wrapped.method("cppsize", &WrappedT::size);
     wrapped.method("cppgetindex", [] (const WrappedT& list, const int i) -> typename WrappedT::const_reference { return list[i]; });
     wrapped.method("cppsetindex!", [] (WrappedT& list, const typename WrappedT::value_type& v, const int i) { list[i] = v; });
     wrapped.method("push_back", static_cast<void(WrappedT::*)(typename WrappedT::parameter_type)>(&WrappedT::push_back));
     wrapped.method("clear", &WrappedT::clear);
     wrapped.method("removeAt", &WrappedT::removeAt);
+  }
+};
+
+// Wrap a ContainerT<KeyT,ValueT>::iterator in a templated struct so it can be added as a parametric type in Julia
+template<template<typename, typename> typename ContainerT, typename KeyT, typename ValueT>
+struct QtIteratorWrapper
+{
+  using iterator_type = typename ContainerT<KeyT, ValueT>::iterator;
+  using value_type = ValueT;
+  using key_type = KeyT;
+  iterator_type value;
+};
+
+template<typename KeyT, typename ValueT> struct QHashIteratorWrapper : QtIteratorWrapper<QHash, KeyT, ValueT> {};
+template<typename KeyT, typename ValueT> struct QMapIteratorWrapper : QtIteratorWrapper<QMap, KeyT, ValueT> {};
+
+template<typename T>
+void validate_iterator(T it)
+{
+  using IteratorT = typename T::iterator_type;
+  if(it.value == IteratorT())
+  {
+    throw std::runtime_error("Invalid iterator");
+  }
+}
+
+struct WrapQtIterator
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    using WrappedT = typename TypeWrapperT::type;
+    using KeyT = typename WrappedT::key_type;
+    using ValueT = typename WrappedT::value_type;
+    
+    wrapped.method("iteratornext", [] (WrappedT it) -> WrappedT { ++(it.value); return it; });
+    wrapped.method("iteratorkey", [] (WrappedT it) -> KeyT { validate_iterator(it); return it.value.key();} );
+    wrapped.method("iteratorvalue", [] (WrappedT it) -> ValueT& { validate_iterator(it); return it.value.value(); } );
+    wrapped.method("iteratorisequal", [] (WrappedT it1, WrappedT it2) -> bool { return it1.value == it2.value; } );
+  }
+};
+
+template<template<typename, typename> typename IteratorWrapperT>
+struct WrapQtAssociativeContainer
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    using WrappedT = typename TypeWrapperT::type;
+    using ValueT = typename WrappedT::mapped_type;
+    using KeyT = typename WrappedT::key_type;
+
+    wrapped.method("cppsize", &WrappedT::size);
+    wrapped.method("cppgetindex", [] (WrappedT& hash, const KeyT& k) -> ValueT& { return hash[k]; });
+    wrapped.method("cppsetindex!", [] (WrappedT& hash, const ValueT& v, const KeyT& k) { hash[k] = v; });
+    wrapped.method("insert", [] (WrappedT& hash, const KeyT& k, const ValueT& v) { hash.insert(k,v); });
+    wrapped.method("clear", &WrappedT::clear);
+    wrapped.method("remove", &WrappedT::remove);
+    wrapped.method("empty", &WrappedT::empty);
+    wrapped.method("iteratorbegin", [] (WrappedT& hash) { return IteratorWrapperT<KeyT,ValueT>{hash.begin()}; });
+    wrapped.method("iteratorend", [] (WrappedT& hash) { return IteratorWrapperT<KeyT,ValueT>{hash.end()}; });
+    wrapped.method("keys", [] (const WrappedT& hash) { return hash.keys(); });
+    wrapped.method("values", &WrappedT::values);
+    wrapped.method("contains", &WrappedT::contains);
   }
 };
 
@@ -230,8 +290,33 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& qml_module)
   qmlRegisterType<qmlwrap::OpenGLViewport>("org.julialang", 1, 0, "OpenGLViewport");
   qmlRegisterType<qmlwrap::MakieViewport>("org.julialang", 1, 0, "MakieViewport");
 
+  // Set pointers to Julia QML module in the classes that use it
   qmlwrap::ApplicationManager::m_qml_mod = qml_module.julia_module();
+  qmlwrap::JuliaItemModel::m_qml_mod = qml_module.julia_module();
   qmlwrap::MakieViewport::m_qml_mod = qml_module.julia_module();
+
+  // Enums
+  qml_module.add_bits<Qt::Orientation>("Orientation", jlcxx::julia_type("CppEnum"));
+  qml_module.set_const("Horizontal", Qt::Horizontal);
+  qml_module.set_const("Vertical", Qt::Vertical);
+
+  qml_module.add_bits<Qt::ItemDataRole>("ItemDataRole", jlcxx::julia_type("CppEnum"));
+  qml_module.set_const("DisplayRole", Qt::DisplayRole);
+  qml_module.set_const("DecorationRole", Qt::DecorationRole);
+  qml_module.set_const("EditRole", Qt::EditRole);
+  qml_module.set_const("ToolTipRole", Qt::ToolTipRole);
+  qml_module.set_const("StatusTipRole", Qt::StatusTipRole);
+  qml_module.set_const("WhatsThisRole", Qt::WhatsThisRole);
+  qml_module.set_const("FontRole", Qt::FontRole);
+  qml_module.set_const("TextAlignmentRole", Qt::TextAlignmentRole);
+  qml_module.set_const("BackgroundRole", Qt::BackgroundRole);
+  qml_module.set_const("ForegroundRole", Qt::ForegroundRole);
+  qml_module.set_const("CheckStateRole", Qt::CheckStateRole);
+  qml_module.set_const("AccessibleTextRole", Qt::AccessibleTextRole);
+  qml_module.set_const("AccessibleDescriptionRole", Qt::AccessibleDescriptionRole);
+  qml_module.set_const("SizeHintRole", Qt::SizeHintRole);
+  qml_module.set_const("InitialSortOrderRole", Qt::InitialSortOrderRole);
+  qml_module.set_const("UserRole", Qt::UserRole);
 
   qml_module.add_type<QObject>("QObject");
   qml_module.add_type<QSize>("QSize")
@@ -287,10 +372,23 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& qml_module)
   auto qvar_type = qml_module.add_type<QVariant>("QVariant");
   qvar_type.method("toString", &QVariant::toString);
 
-  // Add some types that depend on QVariant being mapped
-  qml_module.add_type<QVariantMap>("QVariantMap");
+  qml_module.add_type<QByteArray>("QByteArray").constructor<const char*>()
+    .method("to_string", &QByteArray::toStdString);
+
   qml_module.add_type<Parametric<TypeVar<1>>>("QList", julia_type("AbstractVector"))
-    .apply<QVariantList, QList<QString>, QList<QUrl>>(qmlwrap::WrapQList());
+    .apply<QVariantList, QList<QString>, QList<QUrl>, QList<QByteArray>, QList<int>>(qmlwrap::WrapQList());
+
+  // QMap (= QVariantMap for the given type)
+  qml_module.add_type<Parametric<TypeVar<1>,TypeVar<2>>>("QMapIterator")
+    .apply<qmlwrap::QMapIteratorWrapper<QString, QVariant>>(qmlwrap::WrapQtIterator());
+  qml_module.add_type<Parametric<TypeVar<1>,TypeVar<2>>>("QMap", julia_type("AbstractDict"))
+    .apply<QMap<QString, QVariant>>(qmlwrap::WrapQtAssociativeContainer<qmlwrap::QMapIteratorWrapper>());
+
+  // QHash
+  qml_module.add_type<Parametric<TypeVar<1>,TypeVar<2>>>("QHashIterator")
+    .apply<qmlwrap::QHashIteratorWrapper<int, QByteArray>>(qmlwrap::WrapQtIterator());
+  qml_module.add_type<Parametric<TypeVar<1>,TypeVar<2>>>("QHash", julia_type("AbstractDict"))
+    .apply<QHash<int, QByteArray>>(qmlwrap::WrapQtAssociativeContainer<qmlwrap::QHashIteratorWrapper>());
   
   qml_module.add_type<QQmlPropertyMap>("QQmlPropertyMap", julia_base_type<QObject>())
     .constructor<QObject *>(false)
@@ -379,8 +477,6 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& qml_module)
 
   qml_module.add_type<qmlwrap::JuliaPaintedItem>("JuliaPaintedItem", julia_base_type<QQuickItem>());
 
-  qml_module.add_type<QByteArray>("QByteArray").constructor<const char*>()
-    .method("to_string", &QByteArray::toStdString);
   qml_module.add_type<QQmlComponent>("QQmlComponent", julia_base_type<QObject>())
     .constructor<QQmlEngine*>()
     .method("set_data", &QQmlComponent::setData);
@@ -442,22 +538,29 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& qml_module)
   qml_module.add_type<QPainter>("QPainter")
     .method("device", &QPainter::device);
 
-  qmlwrap::ModelData::m_qml_mod = qml_module.julia_module();
-  qml_module.add_type<qmlwrap::ModelData>("ModelData", julia_base_type<QObject>())
-    .method("remove", &qmlwrap::ModelData::remove)
-    .method("emit_roles_changed", &qmlwrap::ModelData::emit_roles_changed)
-    .method("emit_data_changed", &qmlwrap::ModelData::emit_data_changed)
-    .method("push_back", &qmlwrap::ModelData::push_back)
-    .method("get_julia_data", &qmlwrap::ModelData::get_julia_data);
   qml_module.add_type<QAbstractItemModel>("QAbstractItemModel", julia_base_type<QObject>());
-  qml_module.add_type<QAbstractListModel>("QAbstractListModel", julia_base_type<QAbstractItemModel>());
   qml_module.add_type<QAbstractTableModel>("QAbstractTableModel", julia_base_type<QAbstractItemModel>());
-  qml_module.add_type<qmlwrap::ListModel>("ListModel", julia_base_type<QAbstractListModel>())
-    .constructor<jl_value_t*>()
-    .method("get_model_data", &qmlwrap::ListModel::get_model_data);
-  qml_module.add_type<qmlwrap::TableModel>("TableModel", julia_base_type<QAbstractTableModel>())
-    .constructor<jl_value_t*>()
-    .method("get_model_data", &qmlwrap::TableModel::get_model_data);
+  qml_module.add_type<qmlwrap::JuliaItemModel>("JuliaItemModel", julia_base_type<QAbstractTableModel>())
+    .method("emit_data_changed", &qmlwrap::JuliaItemModel::emit_data_changed)
+    .method("emit_header_data_changed", &qmlwrap::JuliaItemModel::emit_header_data_changed)
+    .method("begin_reset_model", &qmlwrap::JuliaItemModel::begin_reset_model)
+    .method("end_reset_model", &qmlwrap::JuliaItemModel::end_reset_model)
+    .method("begin_insert_rows", &qmlwrap::JuliaItemModel::begin_insert_rows)
+    .method("end_insert_rows", &qmlwrap::JuliaItemModel::end_insert_rows)
+    .method("begin_move_rows", &qmlwrap::JuliaItemModel::begin_move_rows)
+    .method("end_move_rows", &qmlwrap::JuliaItemModel::end_move_rows)
+    .method("begin_remove_rows", &qmlwrap::JuliaItemModel::begin_remove_rows)
+    .method("end_remove_rows", &qmlwrap::JuliaItemModel::end_remove_rows)
+    .method("begin_insert_columns", &qmlwrap::JuliaItemModel::begin_insert_columns)
+    .method("end_insert_columns", &qmlwrap::JuliaItemModel::end_insert_columns)
+    .method("begin_move_columns", &qmlwrap::JuliaItemModel::begin_move_columns)
+    .method("end_move_columns", &qmlwrap::JuliaItemModel::end_move_columns)
+    .method("begin_remove_columns", &qmlwrap::JuliaItemModel::begin_remove_columns)
+    .method("end_remove_columns", &qmlwrap::JuliaItemModel::end_remove_columns)
+    .method("default_role_names", &qmlwrap::JuliaItemModel::default_role_names)
+    .method("get_julia_data", &qmlwrap::JuliaItemModel::get_julia_data);
+
+  qml_module.method("new_item_model", [] (jl_value_t* modeldata) { return jlcxx::create<qmlwrap::JuliaItemModel>(modeldata); });
 
   qml_module.set_override_module(jl_base_module);
   qml_module.method("getindex", [](const QVariantMap& m, const QString& key) { return m[key]; });
